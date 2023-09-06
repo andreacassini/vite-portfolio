c76ea05b576d69e7d9579f2e6711da83ae126764
andreacassini <andrea1316@hotmail.it>
2023-09-06 17:43:22 +0200

    home fonts weight

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,14 +5,17 @@ export default {
 </script>
 
 <template lang="">
-    <div id="jumbotron">
+    <div>
         <div class="container">
             <!-- JUMBOTRON -->
-            <div class="row">
-                <div class="col-12">
+            <div class="row align-items-center" id="jumbotron">
+                <div class="col-6">
                     <h3>Hi, my name is</h3>
-                    <h1>Andrea Cassini.</h1>
-                    <h4>I make stuff for the web.</h4>
+                    <h1 class="fw-bold">Andrea Cassini.</h1>
+                    <h4>I write <span class="fw-bold">&ltscript> </span>for the web.</h4>
+                    <h5>I'm a Full-Stack Developer based in Bordighera, Italy. I enjoy 
+                        learning new ways of coding keep up with technology.
+                    </h5>
                 </div>
             </div>
         </div>
@@ -23,13 +26,19 @@ export default {
     #jumbotron{
         height: calc(100vh - 100px);
         margin-top: 100px;
-        .container{
-            .row{
-                .col-12{
-                    
-                }
+            h3{
+                font-size: 50px;
+            }
+            h1{
+                font-size: 80px;
+            }
+            h4{
+                font-size: 40px;
+                margin-bottom: 20px;
+            }
+            h5{
+                font-size: 28px;
             }
         }
-    }
 
 </style>
